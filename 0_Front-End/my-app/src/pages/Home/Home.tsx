0e2d7f80805dece5f53314a5e545dae4ca1c135a
Jacqueline Sanchez <Jacquelinesanchez4712@gmail.com>
2025-03-18 00:07:59 -0500

    organizing repo

---
+++ 0_Front-End/my-app/src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to Team12's Library!</h1>
+            <p>---This is the homepage.---</p>
+        </div>
+    );
+}
+
+export default Home;
