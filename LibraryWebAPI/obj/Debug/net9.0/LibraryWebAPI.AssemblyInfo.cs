17d63393ba87a2b779e2783332a83a7b7a172d1a
trevrd22 <trevrd22@gmail.com>
2025-03-27 14:53:36 -0500

    bugfixing my eventscalendar

--- LibraryWebAPI/obj/Debug/net9.0/LibraryWebAPI.AssemblyInfo.cs
+++ LibraryWebAPI/obj/Debug/net9.0/LibraryWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a680fa3e0e4e42c178198071cede8d7dd9f025")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d83794baed30de7ca50164810890a7a4d13740b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
