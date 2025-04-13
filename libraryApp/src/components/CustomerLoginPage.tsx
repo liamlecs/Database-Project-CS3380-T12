8e5500ee4d282894a76595194e4941bd1d074ae6
Liam Le <154930237+liamlecs@users.noreply.github.com>
2025-04-13 00:12:52 -0500

    Change Customer Login Page BG picture

--- libraryApp/src/components/CustomerLoginPage.tsx
+++ libraryApp/src/components/CustomerLoginPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../App.css"; // CSS import
-import bgImage from "../assets/library-bg.jpg";
+import bgImage from "../assets/library-bg.png";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
